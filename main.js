a0808f4b23bb0dc96d32583233a99660ace888b9
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-11-26 05:38:20 +0200

    debug server

--- main.js
+++ main.js
@@ -6,22 +6,10 @@ var app = express();
 
 var server = require('http').Server(app);
 
-var io = require('socket.io')(server, {
-  'transports': 'xhr-polling',
-  'polling duration': 10
-});
-
 String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 
 var port = process.env.PORT || 8080;
-
-var bodyParser = require('body-parser');
 app.use(express.static('public'));
-app.use(bodyParser.json());
-
-io.on('connection', function (socket) {
-
-});
 
 app.get('/hello', function(req, res) {
   res.send('hello world')
